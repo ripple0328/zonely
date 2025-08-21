01cbb089ef1a28046f706f7caa48c942570c2f05
Qingbo Zhang <ripple0328@gmail.com>
2025-08-20 21:54:59 -0700

    add seeds

--- lib/zonely/release.ex
+++ lib/zonely/release.ex
@@ -22,6 +22,15 @@ defmodule Zonely.Release do
     Application.fetch_env!(@app, :ecto_repos)
   end
 
+  def seeds do
+    load_app()
+
+    Enum.each(repos(), fn _repo ->
+      # Runs priv/repo/seeds.exs inside the release
+      Code.eval_file(Application.app_dir(@app, "priv/repo/seeds.exs"))
+    end)
+  end
+
   defp load_app do
     # Many platforms require SSL when connecting to the database
     Application.ensure_all_started(:ssl)
