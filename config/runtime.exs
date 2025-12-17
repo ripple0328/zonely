81aa4acb8d10bda7bfc91e85bded9eee8496c3ec
Qingbo Zhang <ripple0328@gmail.com>
2025-12-17 10:43:15 -0800

    Temporarily disable check_origin to debug WebSocket issues

--- config/runtime.exs
+++ config/runtime.exs
@@ -37,12 +37,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    check_origin: [
-      "//#{host}",
-      "//*.#{host}",
-      "https://#{host}",
-      "wss://#{host}"
-    ],
+    check_origin: false,
     secret_key_base: secret_key_base
 
   # Audio cache directory for runtime-generated files
