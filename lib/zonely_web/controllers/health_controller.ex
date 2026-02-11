42dcc527ed3d25e7cbec7b87223f7cea1c58d69b
Qingbo Zhang <ripple0328@gmail.com>
2026-02-10 23:23:18 -0800

    debug: log health endpoints

--- lib/zonely_web/controllers/health_controller.ex
+++ lib/zonely_web/controllers/health_controller.ex
@@ -1,8 +1,11 @@
 defmodule ZonelyWeb.HealthController do
   use ZonelyWeb, :controller
+  require Logger
 
   # Liveness: process is up and responding.
   def healthz(conn, _params) do
+    Logger.info("healthz hit")
+
     conn
     |> put_resp_content_type("text/plain")
     |> send_resp(200, "ok")
@@ -10,6 +13,8 @@ defmodule ZonelyWeb.HealthController do
 
   # Readiness: keep lightweight; expand to include deps (DB/cache) if/when present.
   def readyz(conn, _params) do
+    Logger.info("readyz hit")
+
     conn
     |> put_resp_content_type("text/plain")
     |> send_resp(200, "ready")
