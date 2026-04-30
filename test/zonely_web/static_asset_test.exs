1fd3338ee17a8988c8baabd7961990a94294bd3e
Qingbo Zhang <ripple0328@gmail.com>
2026-04-30 01:53:23 -0700

    Make static asset test checkout-safe

--- test/zonely_web/static_asset_test.exs
+++ test/zonely_web/static_asset_test.exs
@@ -1,17 +1,18 @@
 defmodule ZonelyWeb.StaticAssetTest do
-  use ZonelyWeb.ConnCase, async: true
+  use ZonelyWeb.ConnCase, async: false
 
   test "serves digested root favicon assets", %{conn: conn} do
-    favicon_path =
-      "priv/static/cache_manifest.json"
-      |> File.read!()
-      |> Jason.decode!()
-      |> get_in(["latest", "favicon.svg"])
+    favicon_path = "favicon-test-digest.svg"
+    file_path = Path.join(["priv", "static", favicon_path])
 
-    assert is_binary(favicon_path)
+    File.write!(
+      file_path,
+      ~s(<svg xmlns="http://www.w3.org/2000/svg"><title>Zonely</title></svg>)
+    )
+
+    on_exit(fn -> File.rm(file_path) end)
 
     conn = get(conn, "/" <> favicon_path)
-
     assert response(conn, 200) =~ "<svg"
     assert get_resp_header(conn, "content-type") == ["image/svg+xml"]
   end
